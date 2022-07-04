614492bba0e921dd6c3e7bd1a2653d34ef5fa146
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2022-07-04 22:03:58 +0100

    Project setup

---
+++ Graph/Graph/FlowElement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph
+{
+    public class FlowElement : IElement
+    {
+        public FlowElement Superelement { get; set; }
+        public int Depth { get; set; }
+        public IList<IElement> Subelements { get; set; }
+
+        public FlowElement()
+        {
+            Superelement = null;
+            Depth = 0;
+            Subelements = new List<IElement>();
+        }
+    }
+}
