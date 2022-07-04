80e424da5f60e83a5ca5e0381c5d6f05c8d3f23a
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2022-07-04 22:28:45 +0100

    Added more core elements

---
+++ Graph/Graph/Document.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph
+{
+    public class Document
+    {
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public string Abstract { get; set; }
+        public IList<string> Keywords { get; set; }
+        public string Draft { get; set; }
+        public string Edition { get; set; }
+        public string ISBN { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public IList<Section> Sections { get; set; }
+
+        public Document()
+        {
+            Keywords = new List<string>();
+            PublicationDate = DateTime.Now;
+            Sections = new List<Section>();
+        }
+    }
+}
---
+++ Graph/Graph/Section.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph
+{
+    public class Section : FlowElement
+    {
+        public Document Document { get; set; }
+        public string PageTemplateReference { get; set; }
+        public bool Exclude { get; set; }
+    }
+}
